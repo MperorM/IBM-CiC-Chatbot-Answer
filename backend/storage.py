2da55eef49662434bd95b98a817fd3f70deca7ca
MperorM <mathiaskbonde@gmail.com>
2020-04-03 21:54:10 +0200

    storage returns strings instead of bytes

--- backend/storage.py
+++ backend/storage.py
@@ -1,18 +1,18 @@
 from google.cloud import storage
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
+def upload_blob(bucket_name, data, destination_blob_name):
     """Uploads a file to the bucket.
     Args:
         bucket_name: "your-bucket-name"
-        source_file_name: "local/path/to/file"
         destination_blob_name: "storage-object-name"
+        data: string
     """
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
-    blob.upload_from_filename(source_file_name)
+    blob.upload_from_string(data)
 
 
 def download_blob(bucket_name, source_blob_name):
@@ -25,7 +25,7 @@ def download_blob(bucket_name, source_blob_name):
 
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(source_blob_name)
-    return blob.download_as_string()
+    return blob.download_as_string().decode()
 
 def check_if_exists(bucket_name, source_blob_name):
     storage_client = storage.Client()
