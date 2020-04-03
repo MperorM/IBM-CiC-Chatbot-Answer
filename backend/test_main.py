fc30f521e174347b22bf61e5cf92c4e1c42d930f
MperorM <mathiaskbonde@gmail.com>
2020-04-03 21:55:42 +0200

    Update test_main.py

--- backend/test_main.py
+++ backend/test_main.py
@@ -2,11 +2,12 @@ from unittest.mock import Mock
 import main
 import flask
 
-#TODO: getting weird error when running testsuite, something to do with not mocking flask correctly causing flask.jsonify to fail.
+#     BUG: uuid: 7ec99102-75cc-11ea-af35-acde48001122
+#     description: getting weird error when running testsuite, something to do with not mocking flask correctly causing flask.jsonify to fail.
 def test_hammer():
     message = 'hammer'
     data = {'message': message}
     req = Mock(get_json=Mock(return_value=data), args=data)
 
     # Call tested function
-    assert main.receive_message(req) == "Her kan du finde vores konkurrents bedste hamre: https://www.bauhaus.dk/catalogsearch/result/?q=hammer"
+    assert main.receive_message(req) == "this is not the expected output test should fail"
