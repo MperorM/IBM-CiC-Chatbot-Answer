d22e7f538454d6d55d871d89d2f41e7142efb26b
MperorM <mathiaskbonde@gmail.com>
2020-04-03 23:39:12 +0200

    restructured git into two seperate pages

--- userpage/bewhaos_chatbot/lib/main.dart
+++ userpage/bewhaos_chatbot/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bewhaos A/S',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Bewhaos'),
+      home: MyHomePage(title: 'Bewhaos - Ask Archibald'),
     );
   }
 }
--- userpage/public/flutter_service_worker.js
+++ userpage/public/flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "2ebed1eef93a37fe40e52ad90cf6eb66",
-"/main.dart.js": "77bd432554049952b03482c1166921a9",
+"/main.dart.js": "936976ba0596e4cb075a8fa4936efba0",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
